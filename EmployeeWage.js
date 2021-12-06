22d9e1b2bbdccad12333baa2800e4a835d0c929a
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-06 18:52:16 +0530

    [VIGNESH M] Added: while loop to calculate Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -36,12 +36,27 @@ let empWage1=empHrs1 * WAGE_PER_HOUR;
 console.log("UC3-Employee Wage: "+empWage1)
 
 //UC4-Calculate Wage for a Month
-const NUM_OF_WORKING_DAYS=2;
+const NUM_OF_WORKING_DAYS1=2;
 let empHrs2=0;
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+for(let day=0;day<NUM_OF_WORKING_DAYS1;day++)
 {
     empHrs2 += getWorkingHours(empCheck);
 }
 
 let empWage2=empHrs2 * WAGE_PER_HOUR;
 console.log("UC4-Total Hours: "+empHrs2+" Employee Wage: "+empWage2);
+
+//UC5-Calculate Wage till a condition of total working hours
+// of 160 or max days of 20 is reached for a month
+const MAX_HRS_IN_MONTH=100;
+const NUM_OF_WORKING_DAYS2=10;
+let totalEmpHrs=0;
+let=totalWorkingDays=0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS2)
+{
+    totalWorkingDays++;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage3=totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5-Total Days: "+totalWorkingDays+" Total Hours: "+totalEmpHrs+" Employee Wage: "+empWage3);

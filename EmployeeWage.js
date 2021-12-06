62312e28425c9a620cbdf5c8d237b669a156cffd
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-06 18:27:41 +0530

    [VIGNESH M] Added: getWorkingHours Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,33 +4,32 @@ const IS_PRESENT=1;
 let empCheck=Math.floor(Math.random() * 10) % 3;
 if(empCheck == IS_PRESENT)
 {
-    console.log("Employee is Present");
+    console.log("UC1-Employee is Present");
 }
 else
 {
-    console.log("Employee is Absent");
+    console.log("UC1-Employee is Absent");
 }
 
-//UC2-Ability to Calculate Daily Employee Wage on Part Time and Full Time Employee
+//UC3-Refractor the code with function
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 
-let empHrs=0;
-switch(empCheck)
+function getWorkingHours(empCheck)
 {
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-
-    default:
-        empHrs=0;
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
+empHrs=getWorkingHours(empCheck);
 let empWage=empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage: " + empWage);
+console.log("UC3-Employee Wage: "+empWage)

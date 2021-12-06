4895e5ad4bf5b84824f5827f06d40ad1f057f526
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-12-06 19:18:38 +0530

    [VIGNESH M] Added: empDailyWageArr to store dialy wage in array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -48,15 +48,37 @@ console.log("UC4-Total Hours: "+empHrs2+" Employee Wage: "+empWage2);
 
 //UC5-Calculate Wage till a condition of total working hours
 // of 160 or max days of 20 is reached for a month
-const MAX_HRS_IN_MONTH=100;
+const MAX_HRS_IN_MONTH1=100;
 const NUM_OF_WORKING_DAYS2=10;
-let totalEmpHrs=0;
-let=totalWorkingDays=0;
+let totalEmpHrs1=0;
+let totalWorkingDays1=0;
 
-while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS2)
+while(totalEmpHrs1 <= MAX_HRS_IN_MONTH1 && totalWorkingDays1 < NUM_OF_WORKING_DAYS2)
 {
-    totalWorkingDays++;
-    totalEmpHrs += getWorkingHours(empCheck);
+    totalWorkingDays1++;
+    totalEmpHrs1 += getWorkingHours(empCheck);
 }
-let empWage3=totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5-Total Days: "+totalWorkingDays+" Total Hours: "+totalEmpHrs+" Employee Wage: "+empWage3);
+let empWage3=totalEmpHrs1 * WAGE_PER_HOUR;
+console.log("UC5-Total Days: "+totalWorkingDays1+" Total Hours: "+totalEmpHrs1+" Employee Wage: "+empWage3);
+
+//UC6-Store Daily Wage In Array
+let empHrs3=0;
+function calcDailyWage(empHrs3)
+{
+    return empHrs3 * WAGE_PER_HOUR;
+}
+const MAX_HRS_IN_MONTH2=160;
+const NUM_OF_WORKING_DAYS3=20;
+let totalEmpHrs2=0;
+let totalWorkingDays2=0;
+let empDailyWageArr=new Array();
+
+while(totalEmpHrs2 <= MAX_HRS_IN_MONTH2 && totalWorkingDays2 < NUM_OF_WORKING_DAYS3)
+{
+    totalWorkingDays2++;
+    let empHrs3=getWorkingHours(empCheck);
+    totalEmpHrs2 += empHrs3;
+    empDailyWageArr.push(calcDailyWage(empHrs3));
+}
+let empWage4=calcDailyWage(totalEmpHrs2);
+console.log("UC6-Total Days: "+totalWorkingDays2+" Total Hours: "+totalEmpHrs2+" Employee Wage: "+empWage4);
